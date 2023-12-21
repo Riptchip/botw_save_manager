0c72c493ce2d5084be5cacfb8d101164b7d160c1
Nathan Lima <nathanlima523@hotmail.com>
2023-12-20 22:04:40 -0300

    Detect save type and refactoring

--- main.py
+++ main.py
@@ -7,16 +7,6 @@ from distutils.dir_util import copy_tree
 SWITCH = 'switch'
 WIIU = 'wiiU'
 
-HEADERS = {
-    0x24e2, 0x24EE, 0x2588, 0x29c0,
-    0x3ef8, 0x471a, 0x471b, 0x471e
-}
-
-VERSIONS = {
-    "v1.0", "v1.1", "v1.2", "v1.3",
-    "v1.3.3", "v1.4", "v1.5", "v1.6"
-}
-
 ITEMS = {
     "Item", "Weap", "Armo", "Fire", "Norm", "IceA", "Elec", "Bomb", "Anci", "Anim",
     "Obj_", "Game", "Dm_N", "Dm_A", "Dm_E", "Dm_P", "FldO", "Gano", "Gian", "Grea",
@@ -37,80 +27,106 @@ HASHES = {
 }
 
 class SaveManager():
-    def __init__(self, type, source) -> None:
-        if not os.path.exists(source):
-            raise ValueError('Source folder does not exists')
+    def __init__(self, platform, source) -> None:
+        if not os.path.isdir(source):
+            raise NotADirectoryError('Source path does not exists or is a file')
         
-        self.type = type
+        if not os.path.exists(os.path.join(source, 'option.sav')):
+            raise FileNotFoundError(
+                f"Invalid BotwSave folder: '{source}'\n" +
+                "The selected folder does not contain an 'option.sav' file."
+            )
+
+        self.platform = platform
         self.source = source
         self.readerPos = 0
         
-    def convert(self, destination):
-        if not os.path.exists(destination):
-            raise ValueError('Destination folder does not exists')
+    def get_source_save_type(self):
+        SaveType = {
+            0x0000471B: SWITCH,
+            0x1B470000: WIIU
+        }
         
-        copy_tree(destination, destination + '_backup_' + dt.datetime.now().strftime('%d-%m-%Y_%H.%M'))
+        typeBytes = bytearray(open(os.path.join(self.source, 'option.sav'), 'rb').read(4))
+        typeInt = int.from_bytes(bytes(typeBytes), sys.byteorder, signed=False)
+        return SaveType[min(SaveType.keys(), key=lambda x: abs(typeInt - x))]
+    
+    def get_save_files_in_source(self, dir):
+        filesFound = []
+        
+        for root, dirs, files in os.walk(dir):
+            for file in files:
+                if file.endswith(".sav"):
+                    filesFound.append(os.path.join(root, file))
+        
+        return filesFound
+
+    def item_in_buffer(self, buffer):
+        try:
+            return any([item in buffer.decode('utf-8') for item in ITEMS])
+        except UnicodeDecodeError:
+            return False
+            
+            
+    def convert_file(self, data, file):
+        converted = data.copy()
+        print(file)
+        # pdb.set_trace()
+        
+        normalCount = 0
+        pos = 0
+        while pos < (len(data)/4):
+                
+            if 'trackblock' in file and pos == 0:
+                converted[2:4] = bytes(reversed(converted[4:6]))
+                pos = 2
+                
+            self.readerPos = pos * 4
+            buffer = converted[self.readerPos:self.readerPos+4]
+            
+            # pdb.set_trace()
+            if int.from_bytes(bytes(buffer), sys.byteorder, signed=False) in HASHES:
+                converted[self.readerPos:self.readerPos+4] = bytes(reversed(buffer))
+                pos += 1
+            elif not self.item_in_buffer(buffer):
+                converted[self.readerPos:self.readerPos+4] = bytes(reversed(buffer))
+                normalCount += 1
+            else:
+                pos += 1
+                for i in range(16):
+                    self.readerPos = (pos + (i * 2)) * 4
+                    buffer = converted[self.readerPos:self.readerPos+4]
+                    converted[self.readerPos:self.readerPos+4] = bytes(reversed(buffer))
+                
+                pos += 30
+            
+            pos += 1
+            
+        return converted
+    
+
+    def save_to(self, destination):
+        if not os.path.isdir(destination):
+            raise NotADirectoryError('Destination path does not exists or is a file')
+        
+        if self.platform == self.get_source_save_type():
+            print('Same source and destination is the same platform')
+            return
+        
+        copy_tree(destination, os.path.join(destination + '_backup_' + dt.datetime.now().strftime('%d-%m-%Y_%H.%M')))
         copy_tree(self.source, destination)
         
-        for file in get_files_with_extension(destination, '.sav'):
+        for file in self.get_save_files_in_source(destination):
             data = bytearray(open(file, 'rb').read())
-            converted = data.copy()
-            print(file)
-            # pdb.set_trace()
-            
-            normalCount = 0
-            pos = 0
-            while pos < (len(data)/4):
-                    
-                if 'trackblock' in file and pos == 0:
-                    converted[2:4] = bytes(reversed(converted[4:6]))
-                    pos = 2
-                    
-                self.readerPos = pos * 4
-                buffer = converted[self.readerPos:self.readerPos+4]
+            converted = self.convert_file()
                 
-                # pdb.set_trace()
-                if int.from_bytes(bytes(buffer), sys.byteorder, signed=False) in HASHES:
-                    converted[self.readerPos:self.readerPos+4] = bytes(reversed(buffer))
-                    
-                    pos += 1
-                elif not item_in_buffer(buffer):
-                    converted[self.readerPos:self.readerPos+4] = bytes(reversed(buffer))
-                    normalCount += 1
-                else:
-                    pos += 1
-                    for i in range(16):
-                        self.readerPos = (pos + (i * 2)) * 4
-                        buffer = converted[self.readerPos:self.readerPos+4]
-                        converted[self.readerPos:self.readerPos+4] = bytes(reversed(buffer))
-                    
-                    pos += 30
-                
-                pos += 1
-                
-            open(file, 'wb').write(converted)            
-            
+            open(file, 'wb').write(converted)
 
-
-def item_in_buffer(buffer):
-    try:
-        return any([item in buffer.decode('utf-8') for item in ITEMS])
-    except UnicodeDecodeError:
-        return False
-
-def get_files_with_extension(folder, ext):
-    filesFound = []
-    
-    for root, dirs, files in os.walk(folder):
-        for file in files:
-            if file.endswith(ext):
-                filesFound.append(os.path.join(root, file))
-    
-    return filesFound
 
 if __name__ == '__main__':
     # sm = SaveManager(SWITCH, 'D:\\Users\\Natha\\Emulators\\Wii U Updates\\usr\\save\\00050000\\101c9400\\user\\80000001')
     # sm = SaveManager(SWITCH, 'C:\\Users\\Natha\\Downloads\\yuzu saves')
-    sm = SaveManager(SWITCH, 'test')
-    # sm.convert('test')
-    sm.convert('D:\\Users\\Natha\\Emulators\\Wii U Updates\\usr\\save\\00050000\\101c9400\\user\\80000001')
+    sm = SaveManager(WIIU, 'switch')
+    # print(sm.get_source_save_type())
+    sm.save_to('test')
+    # sm.convert('D:\\Users\\Natha\\Emulators\\Wii U Updates\\usr\\save\\00050000\\101c9400\\user\\80000001')
